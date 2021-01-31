9d8b2b9820194736b1a5ba8742f78dbcd16325a0
Rizal Widyarta Gowandy <rizal.gowandy@tokopedia.com>
2021-01-31 19:10:02 +0700

    Update Terraform and NewRelic Version

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = "~> 0.14.4"
+  required_version = "~> 0.14.5"
 
   required_providers {
     newrelic = {
       source  = "newrelic/newrelic"
-      version = "2.15.1"
+      version = "2.16.0"
     }
   }
 }
