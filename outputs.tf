66f0cf67bd9c4c2a58b430be4d495a84e5b8645d
Rizal Widyarta Gowandy <rizal.gowandy@tokopedia.com>
2021-01-31 22:42:47 +0700

    Comment Out Output

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-output "dashboard_url" {
-  value       = newrelic_dashboard.main.dashboard_url
-  description = "The created dashboard url"
-}
+#output "dashboard_url" {
+#  value       = newrelic_dashboard.main.dashboard_url
+#  description = "The created dashboard url"
+#}
