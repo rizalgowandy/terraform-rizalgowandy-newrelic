043910ed05735acc2d8784d03685f24b6db822c3
Rizal Widyarta Gowandy <rizal.gowandy@tokopedia.com>
2021-02-13 22:52:20 +0700

    Fix Row

--- main.tf
+++ main.tf
@@ -521,7 +521,7 @@ resource "newrelic_one_dashboard" "main" {
 
       content {
         title  = "${var.event_method_substring != "" ? replace(widget_bar.value, var.event_method_substring, var.event_method_replace) : widget_bar.value} - Human error message with most occurrence"
-        row    = 1 + 1 + (widget_bar.key * 3)
+        row    = 3 + 1 + (widget_bar.key * 5)
         column = 7
         width  = 6
 
@@ -555,7 +555,7 @@ resource "newrelic_one_dashboard" "main" {
 
       content {
         title  = "${var.event_method_substring != "" ? replace(widget_bar.value, var.event_method_substring, var.event_method_replace) : widget_bar.value} - Line with most errors"
-        row    = 2 + 1 + (widget_bar.key * 3)
+        row    = 4 + 1 + (widget_bar.key * 5)
         column = 7
         width  = 6
 
